25a2e8cc054bd9d0805d9e7ecfd8a70183b177a8
hruday95 <hruday.95@gmail.com>
2020-03-28 19:13:07 +0530

    latest structure

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-import pandas as pd
-import numpy as np
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
+
 
 
 
