8b8e3fea180db85b3d4b2c71c289d6e99ed8713e
hruday95 <hruday.95@gmail.com>
2020-03-29 11:01:48 +0530

    event data insertion

--- src/engine/event_manager/event_alert.py
+++ src/engine/event_manager/event_alert.py
@@ -1,2 +1,13 @@
 import pandas as pd
+from datetime import datetime
 
+
+class event_manager():
+
+    def __init__(self):
+        self.data = pd.read_excel('src/engine/data/Bday.xlsx', sheet_name='Family')
+
+    def event_finder(self):
+        data = self.data.sort_values('Date')
+        # event_month = data[]
+        # upcoming_events = data[]
