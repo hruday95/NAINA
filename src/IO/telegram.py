ef0d6492ce31be54f987d34829df666ed31e03b5
hruday95 <hruday.95@gmail.com>
2020-04-17 16:29:19 +0530

    user check

--- src/IO/telegram.py
+++ src/IO/telegram.py
@@ -1,5 +1,9 @@
+import pandas as pd
+import os
 import requests
 from bottle import Bottle, response, request as bottle_request
+from src.IO.utils import datatype_identifier
+from src.engine.nainaio import NainaInput, NainaOutput
 
 
 class TelegramIO(Bottle):
@@ -8,6 +12,8 @@ class TelegramIO(Bottle):
 
     def __init__(self, data):
         self.data = data
+        self.type_data = datatype_identifier(data)
+        self.username = ""
 
     def run_reply(self):
         self.param_extractor()
@@ -22,10 +28,21 @@ class TelegramIO(Bottle):
         self.message_text = self.data['message']['text']
         self.sender_name = self.data['message']['from']['first_name']
 
-    def send_reply_to_user(self):
-        answer = "Hello " + self.sender_name + "\n\n\n From NAINA"
+    def send_reply_to_user(self,data):
+        # answer = "Hello " + self.sender_name + "\n\n\n From NAINA"
         json_data = {
             "chat_id": self.chat_id,
-            "text": answer,
+            "text": data,
         }
         return requests.post(self.message_url, json=json_data)
+
+    def user_identification(self):
+        df = pd.read_csv(os.getcwd()+"/src/data/userdata.csv")
+        if self.chat_id in df.T_ID:
+            self.username = df.loc[df.T_ID == self.chat_id , 'Username']
+            return "success"
+        else:
+            data = "Please reply as below\n Note my Name : Your Name \n to let me know you better :)"
+            self.send_reply_to_user(data)
+            return "sent for registration"
+
--- src/engine/naina_brain.py
+++ src/engine/naina_brain.py
@@ -1,4 +1,18 @@
-from engine.event_manager.event_alert import EventManager
+from src.engine.event_manager.event_alert import EventManager
 
+
+class NainaBrain:
+
+    def __init__(self):
+        pass
+
+    def task_classifier(self):
+        pass
+
+    def event_manager(self):
+        pass
+
+    def stocks_brain(self):
+        pass
 
 
